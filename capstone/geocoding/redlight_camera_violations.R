eb446217ca1506606513859089ec066dff2585c7
Leigh Fonseca <leigh@livingdata.us>
2017-04-02 10:58:30 -0700

    formatting updates

--- capstone/geocoding/redlight_camera_violations.R
+++ capstone/geocoding/redlight_camera_violations.R
@@ -6,15 +6,30 @@ colnames(redlight_camera_violations)  [1] <- "intersection"
 
 # create address joinable column
 redlight_camera_violations$address_join <- 
-  gsub(pattern = "$ ST|$ BLVD| RD|$ DR|$ HWY|$ AVE|$ PARK|$  DRIVE", replacement = "", x = redlight_camera_violations$intersection, ignore.case = TRUE) 
+# remove street type from end
+  gsub(pattern = "$ ST|$ BLVD| RD|$ DR|$ HWY|$ AVE|$ PARK|$  DRIVE", replacement = "", x = redlight_camera_violations$intersection, ignore.case = TRUE)
 
+# remove street type and "AND " from string
 redlight_camera_violations$address_join <- 
-  gsub(pattern = " ST AND | BLVD AND | RD AND | DR AND | HWY AND | AVE AND | PARK AND ", replacement = "-", x = redlight_camera_violations$intersection, ignore.case = TRUE) 
+  gsub(pattern = " ST AND | BLVD AND | RD AND | DR AND | HWY AND | AVE AND | PARK AND ", replacement = "-", x = redlight_camera_violations$intersection, ignore.case = TRUE)
 
-redlight_camera_violations$address_join <-
+# remove " AND " from string
+redlight_camera_violations$address_join <- 
   gsub(pattern = " AND ", replacement = "-", x = redlight_camera_violations$address_join, ignore.case = TRUE)
 
+# Create new column for City
 redlight_camera_violations$city <- "Chicago"
+
+# Get distinct list of redlight camera locations for geocoding
+redlight_camera_locations <- redlight_camera_violations %>%
+  select(address_join,city) %>%
+  distinct()
+
+redlight_camera_locations$geo_lookup <- paste(redlight_camera_locations$address_join,redlight_camera_locations$city) 
+
+View(redlight_camera_locations)
+
+rl_coords <- data_frame(redlight_camera_locations)
 
 View(redlight_camera_violations)
 
