29393b0b06df069b27f96561f91ef4fbe3bfe8c3
Leigh Fonseca <leigh@livingdata.us>
2017-04-20 13:21:56 -0700

    moved to scratch

--- data_wrangling_1/scratch.R
+++ data_wrangling_1/scratch.R
@@ -1,4 +1,30 @@
-library(ggmap)
+
+# create a dataframe with the 
+#loc_coord <- data.frame(distinct_locations, speed_coords)
+#colnames(loc_coord) [1] <- "StreetAddress"
+#loc_coord
+#remove(loc_coord)
+
+speed_camera_vio_geo <- inner_join(speed_camera_violations, loc_coord)
+
+
+speed_camera_vl <- merge(x = speed_camera_violations, y = speed_coords, by = "StreetAddress", all = TRUE)
+
+# take a sample and test replacing null lat/lon with geocoded values
+mySample <- speed_camera_vio_geo[sample(1:nrow(speed_camera_vio_geo), 100, replace = FALSE),]
+View(mySample)
+
+mySample$Latitude_2 <- ifelse(!is.na(mySample$Latitude) & )
+pe94.person$foo <- ifelse(!is.na(pe94.person$H01) & pe94.person$H01 == 12, 0, pe94.person$H03)
+
+# create new column and coalesce values
+mySample$Latitude_2 <- replace(mySample$Latitude, is.na(mySample$Latitude), mySample$lat)
+# drop column
+within(mySample, rm("Latitude_2")
+
+
+
+	library(ggmap)
 library(readr)
 library(sp)
 library(rgdal)
@@ -13,12 +39,20 @@ View(speed_camera_violations)
 # rename StreetAddress to remove special character from column name
 colnames(speed_camera_violations) [1] <- "StreetAddress"
 
+# add Chicago as city
+scv_sample$city <- 'Chicago'
+
 # create dataframe of addresses missing latitude
 scv_sample <- sqldf("select distinct StreetAddress, CameraId from speed_camera_violations where Latitude is null", row.names=TRUE)
 #View(scv_sample)
 
-# add Chicago as city
-scv_sample$city <- 'Chicago'
+#Method 2
+# create a data frame with only unique camera_ids that also have missing lat/lon 
+scv_geo <- speed_camera_violations
+
+
+
+
 
 # create column with streetAddress + Chicago for geocoding
 scv_sample$geo <- paste(scv_sample$StreetAddress,'+',scv_sample$city)
@@ -78,3 +112,30 @@ within(mySample, rm("Latitude_2")
 
 # write file with violations, violation type and lat lon
 write.csv(speed_camera_vio_geo, file="~/workspace/found_data_sci/capstone/updated_files/Speed_Camera_Violations.csv", na="")
+
+
+
+# remove street type and "AND " from string
+redlight_camera_violations$address_join <- 
+  gsub(pattern = " ST AND | BLVD AND | RD AND | DR AND | HWY AND | AVE AND | PARK AND ", replacement = "-", x = redlight_camera_violations$intersection, ignore.case = TRUE)
+
+# remove " AND " from string
+redlight_camera_violations$address_join <- 
+  gsub(pattern = " AND ", replacement = "-", x = redlight_camera_violations$address_join, ignore.case = TRUE)
+
+# Create new column for City
+redlight_camera_violations$city <- "Chicago"
+
+# Get distinct list of redlight camera locations for geocoding
+redlight_camera_locations <- redlight_camera_violations %>%
+  select(address_join,city) %>%
+  distinct()
+
+redlight_camera_locations$geo_lookup <- paste(redlight_camera_locations$address_join,redlight_camera_locations$city) 
+
+View(redlight_camera_locations)
+
+rl_coords <- data_frame(redlight_camera_locations)
+
+View(redlight_camera_violations)
+
